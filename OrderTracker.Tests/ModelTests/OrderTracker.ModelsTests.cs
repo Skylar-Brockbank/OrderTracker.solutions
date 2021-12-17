a846ab4d0fb99f460d270322ed79f24b44c4f288
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-12-17 12:59:50 -0800

    pass test 2

--- OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
@@ -12,5 +12,11 @@ namespace OrderTracker.Tests
       Vendor myVendor = new Vendor("Bob");
       Assert.IsNotNull(myVendor);
     }
+    [TestMethod]
+    public void Vendor_assignsNameToSelf_True()
+    {
+      Vendor myVendor = new Vendor("Bob");
+      Assert.AreEqual("Bob",myVendor.Name);
+    }
   }
 }
