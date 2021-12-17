89534c680edacd244f41b1aaaa40bd8e8e5402fd
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-12-17 13:14:07 -0800

    pass test 6 (Vendor.AddOrder)

--- OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
@@ -25,6 +25,14 @@ namespace OrderTracker.Tests
       myVendor.AddOrder("12/17/21",420);
       Assert.AreNotEqual(0,myVendor.OrderList.Count);
     }
+    [TestMethod]
+    public void AddOrder_AddsTheCorrectOrder_True()
+    {
+      Vendor myVendor = new Vendor("Bob");
+      myVendor.AddOrder("12/17/21",420);
+      Assert.AreEqual(myVendor.OrderList[0].OrderDate, "12/17/21");
+      Assert.AreEqual(myVendor.OrderList[0].Amount,420);
+    }
     
 
   }
@@ -34,13 +42,13 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void Order_ConstructorCreatesObject_True()
     {
-      Order myOrder = new Order("12/17/21",15);
+      Order myOrder = new Order("12/17/21",15,1);
       Assert.IsNotNull(myOrder);
     }
     [TestMethod]
     public void Order_ConstructorAssignsCorrectValues_True()
     {
-      Order myOrder = new Order("12/17/21",15);
+      Order myOrder = new Order("12/17/21",15,1);
       Assert.AreEqual("12/17/21",myOrder.OrderDate);
       Assert.AreEqual(15, myOrder.Amount);
     }
