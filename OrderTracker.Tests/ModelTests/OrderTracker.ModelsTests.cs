bb5af194a1144b980b45d217bf1f7526fe965f28
Skylar-Brockbank <skylar.brockbank@gmail.com>
2021-12-23 10:52:19 -0800

    fix tests

--- OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
+++ OrderTracker.Tests/ModelTests/OrderTracker.ModelsTests.cs
@@ -9,35 +9,35 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void Vendor_ConstructorCreatesAnObject_True()
     {
-      Vendor myVendor = new Vendor("Bob");
+      Vendor myVendor = new Vendor("Bob","description");
       Assert.IsNotNull(myVendor);
     }
     [TestMethod]
     public void Vendor_assignsNameToSelf_True()
     {
-      Vendor myVendor = new Vendor("Bob");
+      Vendor myVendor = new Vendor("Bob","description");
       Assert.AreEqual("Bob",myVendor.Name);
     }
     [TestMethod]
     public void AddOrder_AddsAnOrder_True()
     {
-      Vendor myVendor = new Vendor("Bob");
-      myVendor.AddOrder("12/17/21",420);
+      Vendor myVendor = new Vendor("Bob","description");
+      myVendor.AddOrder("12/17/21",420,69,"description");
       Assert.AreNotEqual(0,myVendor.OrderList.Count);
     }
     [TestMethod]
     public void AddOrder_AddsTheCorrectOrder_True()
     {
-      Vendor myVendor = new Vendor("Bob");
-      myVendor.AddOrder("12/17/21",420);
+      Vendor myVendor = new Vendor("Bob","description");
+      myVendor.AddOrder("12/17/21",420,69,"description");
       Assert.AreEqual(myVendor.OrderList[0].OrderDate, "12/17/21");
       Assert.AreEqual(myVendor.OrderList[0].Amount,420);
     }
     [TestMethod]
     public void Find_RetrievesAnOrder_True()
     {
-      Vendor myVendor = new Vendor("Bob");
-      myVendor.AddOrder("12/17/21",420);
+      Vendor myVendor = new Vendor("Bob","description");
+      myVendor.AddOrder("12/17/21",420,69,"description");
       Assert.AreEqual(myVendor.Find(0),myVendor.OrderList[0]);
     }
 
@@ -48,13 +48,13 @@ namespace OrderTracker.Tests
     [TestMethod]
     public void Order_ConstructorCreatesObject_True()
     {
-      Order myOrder = new Order("12/17/21",15,1);
+      Order myOrder = new Order("12/17/21",15,1,0,50,"description");
       Assert.IsNotNull(myOrder);
     }
     [TestMethod]
     public void Order_ConstructorAssignsCorrectValues_True()
     {
-      Order myOrder = new Order("12/17/21",15,1);
+      Order myOrder = new Order("12/17/21",15,1,0,50,"description");
       Assert.AreEqual("12/17/21",myOrder.OrderDate);
       Assert.AreEqual(15, myOrder.Amount);
     }
